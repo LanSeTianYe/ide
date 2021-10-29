8a4abdb33a4d149213df5085bc9abdb1396aaf44
sunfeilong <sunfeilong1993@gmail.com>
2021-10-29 18:03:10 +0800

    json rpc

--- lsp/go.mod
+++ lsp/go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/BurntSushi/toml v0.4.1 // indirect
 	github.com/kr/pretty v0.1.0
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/ybbus/jsonrpc/v2 v2.1.6
+	github.com/sourcegraph/jsonrpc2 v0.1.0
 	go.uber.org/zap v1.19.1
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- lsp/lsp_server.go
+++ lsp/lsp_server.go
@@ -1,73 +1,60 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/kr/pretty"
-	"github.com/ybbus/jsonrpc/v2"
 	"lsp/logger"
+	"net/rpc/jsonrpc"
 )
 
 const address = "http://127.0.0.1:9877"
 
 var log = logger.Get()
 
-type Workspace struct {
-	Name      string `json:"Name"`
-	ModuleDir string `json:"ModuleDir"`
-}
-
-type WorkspaceResponse struct {
-	Workspaces []Workspace `json:"Workspaces"`
-}
-
-type LogTraceParams struct {
-	Message string `json:"message"`
-	Verbose string `json:"verbose,omitempty"`
-}
-
-type CreateFilesParams struct {
-	/**
-	 * An array of all files/folders created in this operation.
-	 */
-	Files []FileCreate `json:"files"`
-}
-
-type FileCreate struct {
-	/**
-	 * A file:// URI for the location of the file/folder being created.
-	 */
-	URI string `json:"uri"`
-}
-
-type ExecuteCommandParams struct {
-	/**
-	 * The identifier of the actual command handler.
-	 */
-	Command string `json:"command"`
-	/**
-	 * Arguments that the command should be invoked with.
-	 */
-	Arguments []json.RawMessage `json:"arguments,omitempty"`
-	WorkDoneProgressParams
-}
-
-type WorkDoneProgressParams struct {
-	/**
-	 * An optional token that a server can use to report work done progress.
-	 */
-	WorkDoneToken ProgressToken `json:"workDoneToken,omitempty"`
-}
-
-type ProgressToken = interface{} /*number | string*/
-
 func main() {
+	//dial, err2 := net.Dial("tcp", "127.0.0.1:9877")
+	//if err2 != nil {
+	//	panic(err2)
+	//}
+	//log.Infof(pretty.Sprint(dial))
+
 	log.Infof("connect to lsp server start")
 	client := jsonrpc.NewClient(address)
 	log.Infof("connect to lsp server success")
+	initializeParams := InitializeParams{}
+	initializeParams.ClientInfo.Name = "test"
+	initializeParams.ClientInfo.Version = "v1.0.0"
+	initializeParams.ProcessID = 2233
+	initializeParams.WorkspaceFolders = []WorkspaceFolder{{Name: "test", URI: "file://test"}}
+	call, err := client.Call("initialize", &initializeParams)
+	if err != nil {
+		log.Errorf("call json rpc method failed. err: %s", err)
+	}
+	log.Infof("initialize: %s", pretty.Sprint(call))
 
-	param := ExecuteCommandParams{}
-	param.Command = "gopls.workspace_metadata"
-	call, err := client.Call("workspace/executeCommand", &param)
+	client = jsonrpc.NewClient(address)
+	call, err = client.Call("initialized", InitializedParams{})
+	if err != nil {
+		log.Errorf("call json rpc method failed. err: %s", err)
+	}
+	log.Infof("call json rpc response. response:%v", pretty.Sprint(call))
+
+	didOpenParam := DidOpenTextDocumentParams{}
+	didOpenParam.TextDocument.URI = "file://test//test.go"
+	didOpenParam.TextDocument.Version = 0
+	didOpenParam.TextDocument.LanguageID = "GO"
+	didOpenParam.TextDocument.Text = "package main\n\nimport \"fmt\"\n\nfunc main(){\n    fmt.Println(\"Hello\")\n}"
+	call, err = client.Call("textDocument/didOpen", &didOpenParam)
+	if err != nil {
+		log.Errorf("call json rpc method failed. err: %s", err)
+	}
+	log.Infof("call json rpc response. response:%v", pretty.Sprint(call))
+
+	param := ReferenceParams{}
+	param.TextDocument.URI = "file://test//test.go"
+	param.Position.Line = 6
+	param.Position.Character = 6
+	param.Context = ReferenceContext{IncludeDeclaration: true}
+	call, err = client.Call("textDocument/references", &param)
 	if err != nil {
 		log.Errorf("call json rpc method failed. err: %s", err)
 	}
