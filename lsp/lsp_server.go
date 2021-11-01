d05c619c148b7ebba73e7ec690f1f6875c75adf4
sunfeilong <sunfeilong1993@gmail.com>
2021-11-01 17:46:02 +0800

    new json rpc client

--- lsp/lsp_server.go
+++ lsp/lsp_server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"github.com/kr/pretty"
+	"github.com/sourcegraph/jsonrpc2"
 	"lsp/logger"
-	"net/rpc/jsonrpc"
+	"net"
 )
 
 const address = "http://127.0.0.1:9877"
@@ -11,52 +13,101 @@ const address = "http://127.0.0.1:9877"
 var log = logger.Get()
 
 func main() {
-	//dial, err2 := net.Dial("tcp", "127.0.0.1:9877")
-	//if err2 != nil {
-	//	panic(err2)
-	//}
-	//log.Infof(pretty.Sprint(dial))
-
+	conn, err2 := net.Dial("tcp", "127.0.0.1:9877")
+	if err2 != nil {
+		panic(err2)
+	}
+	log.Infof(pretty.Sprint(conn))
 	log.Infof("connect to lsp server start")
-	client := jsonrpc.NewClient(address)
+	stream := jsonrpc2.NewBufferedStream(conn, jsonrpc2.VSCodeObjectCodec{})
+	ctx := context.Background()
+	handler := &LSPHandler{}
+	client := jsonrpc2.NewConn(ctx, stream, handler, jsonrpc2.LogMessages(&Logger{}))
 	log.Infof("connect to lsp server success")
 	initializeParams := InitializeParams{}
 	initializeParams.ClientInfo.Name = "test"
 	initializeParams.ClientInfo.Version = "v1.0.0"
 	initializeParams.ProcessID = 2233
 	initializeParams.WorkspaceFolders = []WorkspaceFolder{{Name: "test", URI: "file://test"}}
-	call, err := client.Call("initialize", &initializeParams)
+	response := make(map[string]interface{})
+	err := client.Call(ctx, "initialize", initializeParams, &response, nil)
 	if err != nil {
 		log.Errorf("call json rpc method failed. err: %s", err)
 	}
-	log.Infof("initialize: %s", pretty.Sprint(call))
+	log.Infof("initialize: %s", pretty.Sprint(response))
 
-	client = jsonrpc.NewClient(address)
-	call, err = client.Call("initialized", InitializedParams{})
+	response = make(map[string]interface{})
+	err = client.Call(ctx, "initialized", InitializedParams{}, &response, nil)
 	if err != nil {
 		log.Errorf("call json rpc method failed. err: %s", err)
 	}
-	log.Infof("call json rpc response. response:%v", pretty.Sprint(call))
+	log.Infof("initialized: %s", pretty.Sprint(response))
+
+	//createFilesParams := CreateFilesParams{}
+	//createFilesParams.Files = []FileCreate{FileCreate{URI: "file://test//test.go"}}
+	//response = make(map[string]interface{})
+	//err = client.Call(ctx, "workspace/willCreateFiles", createFilesParams, &response, nil)
+	//if err != nil {
+	//	log.Errorf("call json rpc method failed. err: %s", err)
+	//}
+	//log.Infof("workspace/willCreateFiles: %s", pretty.Sprint(response))
 
 	didOpenParam := DidOpenTextDocumentParams{}
 	didOpenParam.TextDocument.URI = "file://test//test.go"
 	didOpenParam.TextDocument.Version = 0
 	didOpenParam.TextDocument.LanguageID = "GO"
 	didOpenParam.TextDocument.Text = "package main\n\nimport \"fmt\"\n\nfunc main(){\n    fmt.Println(\"Hello\")\n}"
-	call, err = client.Call("textDocument/didOpen", &didOpenParam)
+	response = make(map[string]interface{})
+	err = client.Call(ctx, "textDocument/didOpen", didOpenParam, &response, nil)
 	if err != nil {
 		log.Errorf("call json rpc method failed. err: %s", err)
 	}
-	log.Infof("call json rpc response. response:%v", pretty.Sprint(call))
+	log.Infof("textDocument/didOpen: %s", pretty.Sprint(response))
 
-	param := ReferenceParams{}
-	param.TextDocument.URI = "file://test//test.go"
-	param.Position.Line = 6
-	param.Position.Character = 6
-	param.Context = ReferenceContext{IncludeDeclaration: true}
-	call, err = client.Call("textDocument/references", &param)
+	didSaveTextDocumentParams := DidSaveTextDocumentParams{}
+	didSaveTextDocumentParams.TextDocument.URI = "file://test//test.go"
+	data := "package main\n\nimport \"fmt\"\n\nfunc main(){\n    fmt.Println(\"Hello\")\n}"
+	didSaveTextDocumentParams.Text = &data
+	response = make(map[string]interface{})
+	err = client.Call(ctx, "textDocument/didSave", didSaveTextDocumentParams, &response, nil)
 	if err != nil {
 		log.Errorf("call json rpc method failed. err: %s", err)
 	}
-	log.Infof("call json rpc response. response:%v", pretty.Sprint(call))
+	log.Infof("textDocument/didSave: %s", pretty.Sprint(response))
+
+	didCloseTextDocumentParams := DidCloseTextDocumentParams{}
+	didCloseTextDocumentParams.TextDocument.URI = "file://test//test.go"
+	response = make(map[string]interface{})
+	err = client.Call(ctx, "textDocument/didClose", didCloseTextDocumentParams, &response, nil)
+	if err != nil {
+		log.Errorf("call json rpc method failed. err: %s", err)
+	}
+	log.Infof("textDocument/didClose: %s", pretty.Sprint(response))
+
+	referenceParams := ReferenceParams{}
+	referenceParams.TextDocument.URI = "file://test//test.go"
+	referenceParams.Position.Line = 6
+	referenceParams.Position.Character = 6
+	referenceParams.Context = ReferenceContext{IncludeDeclaration: true}
+	response = make(map[string]interface{})
+	err = client.Call(ctx, "textDocument/references", referenceParams, &response, nil)
+	if err != nil {
+		log.Errorf("call json rpc method failed. err: %s", err)
+	}
+	log.Infof("initialized: %s", pretty.Sprint(response))
+
+}
+
+type LSPHandler struct {
+}
+
+func (l *LSPHandler) Handle(context context.Context, conn *jsonrpc2.Conn, request *jsonrpc2.Request) {
+	log.Infof(pretty.Sprint(request))
+}
+
+type Logger struct {
+}
+
+func (l *Logger) Printf(format string, v ...interface{}) () {
+	log.Infof(format, v...)
 }
